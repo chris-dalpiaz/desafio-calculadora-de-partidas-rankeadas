de792014b35fe13e661598038028f14fb83083b0
Christofer <dalpiazchris@gmail.com>
2023-11-03 09:35:36 -0300

    projeto finalizado

---
+++ index.js
@@ -0,0 +1,30 @@
+function calcularNivel(vitorias, derrotas) {
+    let saldoVitorias = vitorias - derrotas
+    let nivel
+
+    if (saldoVitorias <= 10) {
+        nivel = "Ferro"
+    } else if (saldoVitorias >= 11 && saldoVitorias <= 20) {
+        nivel = "Bronze"
+    } else if (saldoVitorias >= 21 && saldoVitorias <= 50) {
+        nivel = "Prata"
+    } else if (saldoVitorias >= 51 && saldoVitorias <= 80) {
+        nivel = "Ouro"
+    } else if (saldoVitorias >= 81 && saldoVitorias <= 90) {
+        nivel = "Diamante"
+    } else if (saldoVitorias >= 91 && saldoVitorias <= 100) {
+        nivel = "Lendário"
+    } else if (saldoVitorias >= 101) {
+        nivel = "Imortal"
+    } else {
+        nivel = "Nível desconhecido"
+    }
+
+    console.log("O Herói tem saldo de " + saldoVitorias + " e está no nível de " + nivel)
+}
+
+// Exemplo de uso da função
+let vitorias = 75
+let derrotas = 20
+
+calcularNivel(vitorias, derrotas)
